6cbf244ba63b6e5f9941b20647bf4223989c9f6e
Willian Rampazzo <willianrampazzo@users.noreply.github.com>
2026-03-25 17:06:35 -0300

    feat: implement Calculator class and unit tests

---
+++ test_calculator.py
@@ -0,0 +1,32 @@
+import unittest
+from calculator import Calculator
+
+class TestCalculator(unittest.TestCase):
+    def setUp(self):
+        self.calc = Calculator()
+
+    def test_initial_memory(self):
+        self.assertEqual(self.calc.memory, 0.0)
+
+    def test_add(self):
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+
+    def test_subtract(self):
+        self.assertEqual(self.calc.subtract(5, 3), 2)
+        self.assertEqual(self.calc.subtract(3, 5), -2)
+
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(2, 3), 6)
+        self.assertEqual(self.calc.multiply(-2, 3), -6)
+
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(6, 2), 3)
+        self.assertEqual(self.calc.divide(5, 2), 2.5)
+
+    def test_divide_by_zero(self):
+        with self.assertRaises(ValueError):
+            self.calc.divide(5, 0)
+
+if __name__ == '__main__':
+    unittest.main()
